93ae6e2dc8260214d29def7894c89a6cba2e2a3a
Hummertj19 <JPH99@pitt.edu>
2016-09-08 12:42:09 -0400

    Chapter 2 Exercise 7

---
+++ Chapter 2 Examples/Chpt2-Ex7.py
@@ -0,0 +1,17 @@
+# Justin Hummert
+# JPH99@pitt.edu
+# 9/8/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 2, Exercise 7
+
+miles = int(input('How many miles did you drive?'))
+gas = int(input('How many gallons of gas did you use?'))
+kilometers = miles * 1.60934
+liters = gas * 3.78541
+liters_per_100km = 100 * (liters/kilometers)
+print("Kilometers driven:", kilometers)
+print('Liters used:',liters)
+print("Liters per 100km:",liters_per_100km)

ba480c62fee24e69da3682df730d99e244600f1d
Hummertj19 <JPH99@pitt.edu>
2016-09-08 12:34:23 -0400

    Chapter 2 Exercise 5

---
+++ Chapter 2 Examples/Chpt2-Ex5.py
@@ -0,0 +1,19 @@
+# Justin Hummert
+# JPH99@pitt.edu
+# 9/8/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 2, Exercise 5
+
+speed = 90
+time1 = 6
+time2 = 10
+time3 = 15
+time4 = 2 + (5/12)
+distance1 = time1 * speed
+distance2 = time2 * speed
+distance3 = time3 * speed
+distance4 = time4 * speed
+print(distance1,distance2,distance3,distance4)

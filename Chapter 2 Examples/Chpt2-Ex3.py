a4ca4429148b3b355fe6903fb1ea4edb1ec8d9ab
Hummertj19 <JPH99@pitt.edu>
2016-09-08 12:30:23 -0400

    Chapter 2 Exercise 3

---
+++ Chapter 2 Examples/Chpt2-Ex3.py
@@ -0,0 +1,12 @@
+# Justin Hummert
+# JPH99@pitt.edu
+# 9/8/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 2, Exercise 3
+
+square_meters = float(input('Enter total square meters of land:'))
+acres = square_meters / 4046.8564224
+print(acres)

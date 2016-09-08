a675ac42effc89fec38d84122cd2f51621a9c478
Hummertj19 <JPH99@pitt.edu>
2016-09-08 14:32:42 -0400

    Chapter 2 Exercise 9

---
+++ Chapter 2 Examples/Chpt2-Ex9.py
@@ -0,0 +1,12 @@
+# Justin Hummert
+# JPH99@pitt.edu
+# 9/8/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 2, Exercise 9
+
+temperature_in_f = float(input('What is the temperature?'))
+temperature_in_c = 5 *  ((temperature_in_f - 32)/9)
+print(temperature_in_f, temperature_in_c)

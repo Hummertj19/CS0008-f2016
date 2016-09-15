004131251a603342e3624df9db5f0d3951ae4f07
Hummertj19 <JPH99@pitt.edu>
2016-09-15 12:39:34 -0400

    Recitation Exercise 9/15

---
+++ Chapter 3 Exercises/InClassExample.py
@@ -0,0 +1,17 @@
+# Justin Hummert
+# JPH99@pitt.edu
+# 9/15/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python - Recitation 9/15 Exercise
+
+# Taking user's input and calculating answer
+number = int(input('Enter a number '))
+if number > 0:
+    print(number * 3)
+elif number == 0:
+    print('What a cool number!')
+else:
+    print(number + 5)

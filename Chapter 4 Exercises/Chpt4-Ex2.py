d5bb189336c8b2066ce85afb5a4fd6154ea97d1a
Hummertj19 <JPH99@pitt.edu>
2016-09-28 19:21:57 -0400

    Chapter 4 Exercise 2

---
+++ Chapter 4 Exercises/Chpt4-Ex2.py
@@ -0,0 +1,17 @@
+# Justin Hummert
+# JPH99@pitt.edu
+# 9/28/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 4, Exercise 2
+
+# Calcuating calories burned after each minute
+minutes = 10
+calories = 42
+print(calories)
+while minutes < 30:
+    minutes = minutes + 5
+    calories = calories + 21
+    print(calories)

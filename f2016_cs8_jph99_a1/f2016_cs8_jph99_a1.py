ba0487ad4abfdfc2f7c34def19c68bc22f10bbe3
Hummertj19 <JPH99@pitt.edu>
2016-09-29 12:26:59 -0400

    Assignment 1

--- f2016_cs8_jph99_a1/f2016_cs8_jph99_a1.py
+++ f2016_cs8_jph99_a1/f2016_cs8_jph99_a1.py
@@ -8,21 +8,21 @@
 # Assignment Number 1
 
 # Asking user for USC or Metric, and then converting
-system = (input('Enter + for USC or - for Metric units.'))
+system = (input('Enter + for USC or - for Metric units. '))
 if system != '+':
     if system != '-':
         print('Error')
         quit()
 if system == '+':
-    distance_us = float(input('How far did you drive?'))
-    gas_us = float(input('How much gas did you use?'))
+    distance_us = float(input('How far did you drive? '))
+    gas_us = float(input('How much gas did you use? '))
     distance_metric = distance_us * 1.60934
     gas_metric = gas_us * 3.78541
     us_mpg = distance_us/gas_us
     metric_lpok = (100 * gas_metric)/distance_metric
 else:
-    distance_metric = float(input('How far did you drive?'))
-    gas_metric = float(input('How much gas did you use?'))
+    distance_metric = float(input('How far did you drive? '))
+    gas_metric = float(input('How much gas did you use? '))
     distance_us = distance_metric * 0.621371
     gas_us = gas_metric * 0.264172
     metric_lpok = (100 * gas_metric)/distance_metric

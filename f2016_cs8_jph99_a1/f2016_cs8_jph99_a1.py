8163b8cd3953fcaf7d4edd258bc7bdf1d5ea2d10
Hummertj19 <JPH99@pitt.edu>
2016-09-28 20:08:51 -0400

    Assignment 1

---
+++ f2016_cs8_jph99_a1/f2016_cs8_jph99_a1.py
@@ -0,0 +1,35 @@
+# Justin Hummert
+# JPH99@pitt.edu
+# 9/30/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Assignment Number 1
+
+# Asking user for USC or Metric, and then converting
+system = (input('Enter + for USC or - for Metric units.'))
+if system != '+':
+    if system != '-':
+        print('Error')
+        quit()
+if system == '+':
+    distance_us = float(input('How far did you drive?'))
+    gas_us = float(input('How much gas did you use?'))
+    distance_metric = distance_us * 1.60934
+    gas_metric = gas_us * 3.78541
+    us_mpg = distance_us/gas_us
+    metric_lpok = (100 * gas_metric)/distance_metric
+else:
+    distance_metric = float(input('How far did you drive?'))
+    gas_metric = float(input('How much gas did you use?'))
+    distance_us = distance_metric * 0.621371
+    gas_us = gas_metric * 0.264172
+    metric_lpok = (100 * gas_metric)/distance_metric
+    us_mpg = distance_us/gas_us
+
+# Displaying Data
+print('                USC     Metric')
+print('Distance     :',format(distance_us,'6,.3f'),format(distance_metric,'6,.3f'))
+print('Gas          :',format(gas_us,'6.3f'),format(gas_metric,'6.3f'))
+print('Consumption  :',format(us_mpg,'6.3f'),format(metric_lpok,'6.3f'))

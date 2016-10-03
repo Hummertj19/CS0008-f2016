98a1c9a6391813a34b6f28cfef9b89e89bb76200
Hummertj19 <JPH99@pitt.edu>
2016-10-03 19:33:59 -0400

    Chapter 4 Exercise 6

---
+++ Chapter 4 Exercises/Chpt4-Ex6.py
@@ -0,0 +1,13 @@
+# Justin Hummert
+# JPH99@pitt.edu
+#10/3/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 4, Exercise 6
+
+# Taking Celcius and converting to Fahrenheit, then displaying results
+for celsius in range(0, 21):
+    fahrenheit = (celsius*(9/5))+32
+    print(celsius,fahrenheit)

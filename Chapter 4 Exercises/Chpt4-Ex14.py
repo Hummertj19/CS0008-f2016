e644ac41c3cae709694d6593b8e0ebc0a66691b4
Hummertj19 <JPH99@pitt.edu>
2016-10-05 18:46:22 -0400

    Chapter 4 Exercise 14

---
+++ Chapter 4 Exercises/Chpt4-Ex14.py
@@ -0,0 +1,17 @@
+# Justin Hummert
+# JPH99@pitt.edu
+# 10/5/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 4, Exercise 14
+
+#
+num_steps = 6
+
+for r in range(num_steps):
+    print('#')
+    for c in range(0,r+1):
+        print(' ',end='')
+    print('#')

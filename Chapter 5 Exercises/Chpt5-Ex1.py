d4bfd8743e08994a02eec70878d349c5f0d362ae
Hummertj19 <JPH99@pitt.edu>
2016-10-12 16:10:20 -0400

    Chapter 5 Exercise 1

---
+++ Chapter 5 Exercises/Chpt5-Ex1.py
@@ -0,0 +1,17 @@
+# Justin Hummert
+# JPH99@pitt.edu
+# 10/12/16
+# CS0008-f2016
+# Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 5, Exercise 1
+
+# Asks user to enter miles, then converts to kilometers in the function
+
+
+def converter():
+    miles = kilometers * 0.6214
+    print(miles)
+kilometers = float(input('Enter kilometers: '))
+converter()
